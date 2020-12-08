1810623d06cefe4497daee677394e88f77f9ce7b
sfaneleMabaso <38728857+sfaneleMabaso@users.noreply.github.com>
2020-12-08 15:55:48 +0200

    Update Task5

--- Task5.js
+++ Task5.js
@@ -9,5 +9,5 @@ var x = prompt ("Enter first number: ");
 var y = prompt ("Enter second number: ");
 var z = prompt ("Enter third number: ");
 
-console.log(TaskFive(x,y,z));
+console.log(taskFive(x,y,z));
 //console.log(A);
