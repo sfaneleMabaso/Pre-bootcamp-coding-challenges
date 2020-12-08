f3a31320bd28d4c72988b6f4b223ea8e2d219576
sfaneleMabaso <38728857+sfaneleMabaso@users.noreply.github.com>
2020-12-08 15:52:33 +0200

    Update task4

--- Task4.js
+++ Task4.js
@@ -1,6 +1,6 @@
 function taskFour(x , y)
 {
-  if ((x == 3 || y == 3) && ( x + y == 3))
+  if ((x == 3 || y == 3) && (( x + y)  == 3))
   return true;
   else
   return false;
@@ -8,5 +8,5 @@ function taskFour(x , y)
 
 var x = prompt ("Enter first number: ");
 var y = prompt ("Enter second number: ");
-var results = TaskFour(x , y);
+var results = taskFour(x , y);
 console.log( results);
